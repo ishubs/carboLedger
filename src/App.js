aa494ed79c8d9b65c1901064d03f55cacef154f1
Shubham Giri <shubhamgiri@Shubhams-Air-2.mshome.net>
2023-04-26 23:37:02 +0530

    assignment

--- src/App.js
+++ src/App.js
@@ -4,20 +4,196 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="table-wrapper">
+        <table>
+          <thead>
+            <tr>
+              <th>Column 1</th>
+              <th>Column 2</th>
+              <th>Column 3</th>
+              <th>Column 4</th>
+              <th>Column 5</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+            <tr>
+              <td>Row 2, Column 1</td>
+              <td>Row 2, Column 2</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+              <td>Row 1, Column 1</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
